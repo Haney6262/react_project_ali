993ab1c2de55c75a91a45af84f67f2161e997656
Haney6262 <qb202101011@wmsu.edu.ph>
2025-05-05 17:02:56 +0800

    change

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './NavBar';
 import Hero from './hero';
-import About from './about';
+import About from './About';
 import './App.css';
 
 function App() {
